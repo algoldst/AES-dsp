5e73c0fdd7f63e2f53cfb9e9690b2032cf534847
Alex Goldstein <algoldst@calpoly.edu>
2020-04-25 01:41:46 -0700

    Symmetric pz filter
    - Change show_filter_responses_pz.m to accept single poles/zeros. It will automatically create the conjugate pair.

--- show_filter_response_pz.m
+++ show_filter_response_pz.m
@@ -3,6 +3,8 @@ function [Bk,Ak,HF,Fd,hn,n]=show_filter_response_pz(zero,pole,K,fsample,num_of_f
 % where the arguments are:
 % poles = a list of the Z plane locations (complex values) for all the POLES of the filter (a row vector)
 % zeros = a list of the Z plane locations (complex values) for all the ZEROS of the filter (a row vector)
+%               Only enter one of each pole or zero pair. The function will
+%               create a symmetric conjugate on the negative complex plane.
 % K = Multiplier constant for the transfer function (which you should multiply H(z) by)
 % fsample = sampling frequency (samples / second)
 % num_of_f_points = the # of points for the freq. response plot
@@ -19,8 +21,8 @@ function [Bk,Ak,HF,Fd,hn,n]=show_filter_response_pz(zero,pole,K,fsample,num_of_f
 %% Function Placeholders (for test/debug)
 % Comment this section out when finished debugging.
 % clear all; format shortg;
-% zero = [-1 j -j]
-% pole = [0]
+% zero = [-1 j]
+% pole = [.2+.3*j]
 % K=1.1
 % figure_num = 1;
 % num_of_f_points = 25e3;
@@ -29,6 +31,33 @@ function [Bk,Ak,HF,Fd,hn,n]=show_filter_response_pz(zero,pole,K,fsample,num_of_f
 
 
 %% BEGIN FUNCTION
+
+% Create symmetry by mirroring all poles/zeros to opposite side of the 
+% complex plane.
+% Zeros
+k = 1;
+zero_conj = [];
+for N = 1:length(zero)
+    if mod(angle(zero(N)),pi) ~= 0
+        mod(angle(zero(N)),pi)
+        zero_conj(k) = conj(zero(N))
+        k = k+1
+    end
+end
+% Poles
+k = 1;
+pole_conj = [];
+for N = 1:length(pole)
+    if mod(angle(pole(N)),pi) ~= 0
+        mod(angle((N)),pi)
+        pole_conj(k) = conj(pole(N))
+        k = k+1
+    end
+end
+
+% Create full zero/pole array
+zero = [zero zero_conj];
+pole = [pole pole_conj];
 
 % Find Ak and Bk from pole/zero
 Bk = poly(zero)
